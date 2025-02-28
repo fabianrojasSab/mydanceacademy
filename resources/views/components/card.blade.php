09bc1b4e814c0fe54f201a533ddb126a988611e6
DesarrolloWeb23 <Desarrollo1@pijaossalud.com.co>
2025-02-28 15:35:21 -0500

    style
    Se realiza cambios en la vista, cambiando el estilo de las tarjetas de la dashboard

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,15 +1,16 @@
-<div class="max-w-sm mt-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
-    <div class="p-5">
-        <a href="#">
+<div class="max-w-sm mt-5 bg-white shadow-lg p-6 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-xl transition rounded-xl cursor-pointer">
+    <div class="grid lg:grid-cols-3 grid-cols-1 md:container md:mx-auto">
+        <div class="lg:col-span-2 col-1 h-auto ">
+            <a href="#">
+                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+            </a>
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$tittle}}</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$description}}</p>
-        <button  href="{{ route($ruta) }}" wire:navigate
-        class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button">Ir</button>
-        </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$description}}</p>
+        </div>
+        <div class="lg:col-auto col-1 flex items-center justify-center">
+            <button  href="{{ route($ruta) }}" wire:navigate
+            class="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full transition"
+            type="button">Ir</button>
+        </div>
     </div>
 </div>
