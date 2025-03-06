806269e8b607167f97d3f9ee8dbf8c2b214597e4
DesarrolloWeb23 <Desarrollo1@pijaossalud.com.co>
2025-03-06 12:54:42 -0500

    style
    se corrige para centrar componentes

--- resources/views/livewire/inscriptions.blade.php
+++ resources/views/livewire/inscriptions.blade.php
@@ -43,7 +43,7 @@
         </section>
         
         <section class="lg:col-span-2 col-1 h-auto p-4">   
-            <div class="h-full pt-6 px-2 rounded-lg bg-white">
+            <div class="h-full px-6 rounded-lg bg-white shadow-lg">
                 <div class="rounded-t mb-0 px-4 py-3 border-0">
                     <div class="flex flex-wrap items-center">
                         <div x-data="{ allStudents: @entangle('allStudents') }" class="relative w-full px-4 max-w-full flex-grow flex-1">
